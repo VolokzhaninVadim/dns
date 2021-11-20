9c511abc01e1830ae5cdaf4a7ef75e3d4f6d9a50
Volokzhanin Vadim <volokzhanin@yandex.ru>
2021-11-20 13:34:08 +1000

    первое помещение

---
+++ pihole_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Создаем резервную копию
+cd /home/ubuntu/docker/pihole/
+tar cvpzf /mnt/backup/backup/vvy_pihole/"$(date '+%Y-%m-%d').tar.gz" ./
+# Удаляем архивы резервной копии старше n дней
+find /mnt/backup/backup/vvy_pihole/ -mtime +3 -type f -delete
+
+# restore 
+# cd /home/ubuntu/docker/pihole/ & tar xpvzf /mnt/backup/backup/vvy_pihole/2021-10-09.tar.gz
+
+
