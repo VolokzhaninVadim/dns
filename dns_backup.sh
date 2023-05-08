ad04d7cae9ccf3239dd76aad4889c5a2a744b83c
Volokzhanin Vadim <volokzhanin@yandex.ru>
2023-05-08 14:07:55 +0300

    add p7zip

--- dns_backup.sh
+++ dns_backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+#sudo apt-get install p7zip-full
 # Создаем резервную копию
 cd /home/volokzhanin/docker/dns/
 tar cvpzf /mnt/backup/backup/vvy_dns/"$(date '+%Y-%m-%d').tar.gz" ./
+7za a -tzip -p$PASSWORD_ARCHIVE_BACKUP_DOCKER -mem=AES256  /mnt/backup/backup/vvy_dns/"$(date '+%Y-%m-%d').zip" /mnt/backup/backup/vvy_dns/"$(date '+%Y-%m-%d').tar.gz"
+rm /mnt/backup/backup/vvy_dns/"$(date '+%Y-%m-%d').tar.gz"
 # Удаляем архивы резервной копии старше n дней
 find /mnt/backup/backup/vvy_dns/ -mtime +3 -type f -delete
 
-# restore 
+# restore
+# 7za e /mnt/backup/backup/vvy_dns/2021-10-09.zip
 # cd /home/volokzhanin/docker/dns/ & tar xpvzf /mnt/backup/backup/vvy_dns/2021-10-09.tar.gz
-
 
