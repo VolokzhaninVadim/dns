e838d08038b5147b07fa8db826337eaee11124d8
Volokzhanin Vadim <volokzhanin@yandex.ru>
2023-07-01 16:01:31 +0300

    add gpg encrypt and descrypt

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Create variables
+FILE=$(date '+%Y-%m-%d_%H_%M_%S.tgz')
+DIRECTORY='/mnt/backup/backup/dns'
+GPG_KEY=634064C6
+
+# Create archive
+tar --create \
+    --gzip \
+    --file=$DIRECTORY/$FILE \
+    --ignore-failed-read \
+    --preserve-permissions \
+    --verbose \
+'.'
+
+# Create encrypted archive
+gpg --recipient $GPG_KEY \
+    --symmetric \
+    --batch \
+    --passphrase $GPG_PASSPHRASE \
+    --encrypt $DIRECTORY/$FILE
+rm $DIRECTORY/$FILE
+
+# Delete files older than n days
+find $DIRECTORY -mtime +0 -type f -delete
