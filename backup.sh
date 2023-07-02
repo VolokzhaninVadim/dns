f76e358bbc99c8be7e27ab78234e5127fb8c7f6b
Volokzhanin Vadim <volokzhanin@yandex.ru>
2023-07-02 16:24:18 +0300

    add DIRECTORY_SOURCE, DIRECTORY_TARGET

--- backup.sh
+++ backup.sh
@@ -2,25 +2,27 @@
 
 # Create variables
 FILE=$(date '+%Y-%m-%d_%H_%M_%S.tgz')
-DIRECTORY='/mnt/backup/backup/dns'
+DIRECTORY_SOURCE='/home/volokzhanin/server/repos/cloud/dns'
+DIRECTORY_TARGET='/mnt/backup/backup/dns'
+
 GPG_KEY=634064C6
 
 # Create archive
 tar --create \
     --gzip \
-    --file=$DIRECTORY/$FILE \
+    --file=$DIRECTORY_TARGET/$FILE \
     --ignore-failed-read \
     --preserve-permissions \
     --verbose \
-'/home/volokzhanin/docker/dns/'
+$DIRECTORY_SOURCE
 
 # Create encrypted archive
 gpg --recipient $GPG_KEY \
     --symmetric \
     --batch \
     --passphrase $GPG_PASSPHRASE \
-    --encrypt $DIRECTORY/$FILE
-rm $DIRECTORY/$FILE
+    --encrypt $DIRECTORY_TARGET/$FILE
+rm $DIRECTORY_TARGET/$FILE
 
 # Delete files older than n days
-find $DIRECTORY -mtime +0 -type f -delete
+find $DIRECTORY_TARGET -mtime +0 -type f -delete
