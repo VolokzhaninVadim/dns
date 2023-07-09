8198b79adfb1ed12c422e5891ee2bd3d501848e6
Volokzhanin Vadim <volokzhanin@yandex.ru>
2023-07-09 12:11:37 +0300

    add project name

--- backup.sh
+++ backup.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
-# Create variables
+echo $(date '+%Y-%m-%d %H %M %S') 'Create variables'
 FILE=$(date '+%Y-%m-%d_%H_%M_%S')
-ARCHIVE_TYPE='.tgz'
-GPG_TYPE='.gpg'
-DIRECTORY_SOURCE='/home/volokzhanin/server/repos/cloud/dns'
-DIRECTORY_TARGET='/mnt/backup/backup/dns'
-DIRECTORY_S3='/mnt/s3/backup/dns'
-
+ARCHIVE_TYPE='tgz'
+GPG_TYPE='gpg'
+PROJECT='dns'
+DIRECTORY_SOURCE='/home/volokzhanin/server/repos/cloud/'$PROJECT
+DIRECTORY_TARGET='/mnt/backup/backup/backup'
+DIRECTORY_S3='/mnt/s3/backup/'$PROJECT
 GPG_KEY=634064C6
 
-# Create archive
+echo $(date '+%Y-%m-%d %H %M %S') 'Create archive'
 tar --create \
     --gzip \
-    --file=$DIRECTORY_TARGET/$FILE$ARCHIVE_TYPE \
+    --file=$DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE \
     --ignore-failed-read \
     --preserve-permissions \
     --verbose \
 $DIRECTORY_SOURCE
 
-# Create encrypted archive
+echo $(date '+%Y-%m-%d %H %M %S') 'Create encrypted archive'
 gpg --recipient $GPG_KEY \
     --symmetric \
     --batch \
     --passphrase $GPG_PASSPHRASE \
-    --encrypt $DIRECTORY_TARGET/$FILE$ARCHIVE_TYPE
-rm $DIRECTORY_TARGET/$FILE$ARCHIVE_TYPE
+    --encrypt $DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE
+rm $DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE
 
-# Move file
-mv $DIRECTORY_TARGET/$FILE$ARCHIVE_TYPE$GPG_TYPE $DIRECTORY_S3
+echo $(date '+%Y-%m-%d %H %M %S') 'Move file'
+mv $DIRECTORY_TARGET/$FILE'_'$PROJECT'_''.'$ARCHIVE_TYPE'.'$GPG_TYPE $DIRECTORY_S3
 
-# Delete files older than n days
-find $DIRECTORY_S3 -mtime +20 -type f -delete
+echo $(date '+%Y-%m-%d %H %M %S') 'Delete files older than n days'
+find $DIRECTORY_S3 -mtime +20 \
+    -type f \
+    -delete
