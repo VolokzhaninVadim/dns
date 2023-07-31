cf2ebc8fbfc367cf243ece72f9a19ec39557a3b2
Volokzhanin Vadim <volokzhanin@yandex.ru>
2023-07-31 18:42:15 +0300

    change archive type

--- unzip.sh
+++ unzip.sh
@@ -2,20 +2,21 @@
 
 # Create variables
 GPG_KEY=634064C6
-
+GPG_PASSPHRASE=/home/volokzhanin/.gnupg/backup_passphrase
 
 # Decrypt
-for i in *tgz.gpg;
+for i in *zst.gpg;
 do
 gpg --decrypt \
     --batch \
     --passphrase $GPG_PASSPHRASE \
     --recipient $GPG_KEY \
-    "$i" > "$i".tgz;
+    --passphrase-file $GPG_PASSPHRASE \
+    "$i" > "$i".tar.zst;
 done;
 
 # Unzip
-for i in *.tgz;
+for i in *.tar.zst;
 do
 tar --preserve-permissions \
     --extract \
